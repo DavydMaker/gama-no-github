a1b4b3c8165ce6661aa8fabd2768d1940d28c7b5
Davyd Maker <davydmaker@gmail.com>
2020-10-17 17:56:37 -0300

    [22] Gabriel Ramos - JavaScript | Optimizndo resposta do exercício 1 de numbers

--- transformadores/gabriel-ramos/javascript/exercicios/dia-1/numbers/exercicio1.js
+++ transformadores/gabriel-ramos/javascript/exercicios/dia-1/numbers/exercicio1.js
@@ -8,4 +8,4 @@
 
 const valor = 123.456;
 
-console.log('R$ ' + valor.toFixed(2).replace('.', ',')); // Deverá exibir R$123,45
+console.log(`R$ ${valor.toFixed(2).replace('.', ',')}`); // Deverá exibir R$123,45
